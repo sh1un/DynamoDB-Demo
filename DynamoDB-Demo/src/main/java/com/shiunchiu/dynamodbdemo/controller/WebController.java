52ee18482fec4a25c0239e164b7bcd44b6842f2a
sh1un <a0923183408@gmail.com>
2023-07-30 17:48:03 +0800

    feat(WebController): Add getUserList & deleteUserById API endpoints

--- DynamoDB-Demo/src/main/java/com/shiunchiu/dynamodbdemo/controller/WebController.java
+++ DynamoDB-Demo/src/main/java/com/shiunchiu/dynamodbdemo/controller/WebController.java
@@ -3,10 +3,12 @@ package com.shiunchiu.dynamodbdemo.controller;
 import com.shiunchiu.dynamodbdemo.model.User;
 import com.shiunchiu.dynamodbdemo.repository.UserRepository;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +37,24 @@ public class WebController {
     Optional<User> user = userRepository.findById(userId);
 
     return ResponseEntity.status(HttpStatus.OK).body(user);
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity<List<User>> getUserList() {
+    List<User> userList = (List<User>) userRepository.findAll();
+
+    return ResponseEntity.status(HttpStatus.OK).body(userList);
+
+  }
+
+  @DeleteMapping("/users/{userId}")
+  public ResponseEntity<String> deleteUserById(@PathVariable String userId) {
+    if (userRepository.findById(userId).isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No content");
+    }
+
+    userRepository.deleteById(userId);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body("deleted");
   }
 
 }
