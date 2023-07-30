f666e8fb727d7f150ccab970a1317f253bd14c7e
sh1un <a0923183408@gmail.com>
2023-07-30 14:56:03 +0800

    first commit

---
+++ DynamoDB-Demo/src/main/java/com/shiunchiu/dynamodbdemo/DynamoDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shiunchiu.dynamodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbDemoApplication.class, args);
+	}
+
+}
